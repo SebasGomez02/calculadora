48439c0c8d8376724be21bad7fa3df7fa83ee112
SebasGomez02 <sgomez@optimaingenieria.co>
2025-05-27 09:53:31 -0500

    Este es un mensaje de commit de ejemplo
    Seria el segundo commit de la rama 'second' donde vemos los tipos de
    strings que podemos usar en Python aun con algunos por ensayar

--- hello.py
+++ hello.py
@@ -2,3 +2,18 @@
 print("Hola, mundo!")
 # Este es un comentario en Python
 
+"""
+este es un comentario en Python
+Este es un comentario de varias líneas
+Puedes usar comillas triples para comentarios de varias líneas
+Puedes usar comillas simples o dobles para comentarios de una línea
+Puedes usar el símbolo de número para comentarios de una línea
+Puedes usar el símbolo de almohadilla para comentarios de una línea
+"""
+print(type("Soy tipo str")) # Tipo de dato <class 'str'>
+print(type(5)) # Tipo de dato <class 'int'>
+print(type(5.0)) # Tipo de dato <class 'float'>
+print(type(True)) # Tipo de dato <class 'bool'>
+print(type(None)) # Tipo de dato <class 'NoneType'>
+
+
